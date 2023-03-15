7d702ae3faab2cc357ba42bc661905d525e331a4
santiago <santysjr956@gmail.com>
2023-03-15 15:00:05 -0300

    firt commit ecommerce

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TestinSpringSecurity
+# ecommerce
--- src/main/java/com/example/appEcomerce/AppEcomerce.java
+++ src/main/java/com/example/appEcomerce/AppEcomerce.java
@@ -1,4 +1,4 @@
-package com.example.appTestSpringSecurity;
+package com.example.appEcomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
